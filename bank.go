72d1815aa3c7c149c001b4cded2149caa7fee0b4
Takumi Kanada <candy12t@gmail.com>
2021-03-23 15:32:10 +0900

    feat: Withdraw

--- bank.go
+++ bank.go
@@ -27,3 +27,17 @@ func (a *Account) Deposit(amount float64) error {
 	a.Balance += amount
 	return nil
 }
+
+// Withdraw ...
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to withdraw should be greater than zero")
+	}
+
+	if a.Balance < amount {
+		return errors.New("the amount to withdraw should be greater than the account's balance")
+	}
+
+	a.Balance -= amount
+	return nil
+}
