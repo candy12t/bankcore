cae8847845ee27f93289c320da88a75bfb504110
Takumi Kanada <candy12t@gmail.com>
2021-03-23 15:30:53 +0900

    feat: Deposit

--- bank.go
+++ bank.go
@@ -1,5 +1,9 @@
 package bank
 
+import (
+	"errors"
+)
+
 // Customer ...
 type Customer struct {
 	Name    string
@@ -12,4 +16,14 @@ type Account struct {
 	Customer
 	Number  int32
 	Balance float64
+}
+
+// Deposit ...
+func (a *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("the amount to deposit should be greater than zero")
+	}
+
+	a.Balance += amount
+	return nil
 }
