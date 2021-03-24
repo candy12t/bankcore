3ad60862eb8914d0ce3585101eac603f1a2f8417
Takumi Kanada <candy12t@gmail.com>
2021-03-24 15:42:43 +0900

    feat: transfer

--- bank.go
+++ bank.go
@@ -47,3 +47,18 @@ func (a *Account) Withdraw(amount float64) error {
 func (a *Account) Statement() string {
 	return fmt.Sprintf("%v - %v - %v", a.Number, a.Name, a.Balance)
 }
+
+// Transfer ...
+func (a *Account) Transfer(amount float64, dest *Account) error {
+	if amount <= 0 {
+		return errors.New("the amount to withdraw should be greater than zero")
+	}
+
+	if a.Balance < amount {
+		return errors.New("the amount to withdraw should be greater than the account's balance")
+	}
+
+	a.Withdraw(amount)
+	dest.Deposit(amount)
+	return nil
+}
